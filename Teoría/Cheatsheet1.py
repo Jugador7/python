492d414e4ddaaf70642c9cd4d6b0b5a8bb67d77f
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-04-11 19:21:27 -0500

    Update Cheatsheet1.py

--- Teoría/Cheatsheet1.py
+++ Teoría/Cheatsheet1.py
@@ -2,3 +2,64 @@
 concatenated_string = string1 + string2 
 result = "Hello" + " John"</td>
 
+#Data Types
+x=7 
+# Integer Value 
+y=12.4 
+# Float Value 
+is_valid = True 
+# Boolean Value 
+is_valid = False 
+# Boolean Value 
+F_Name = "John" 
+# String Value
+
+#Indexing
+my_string="Hello" 
+char = my_string[0]
+
+#Len
+my_string="Hello" 
+length = len(my_string)
+
+#Lower
+my_string="Hello" 
+uppercase_text = my_string.lower()
+
+#Print
+print("Hello, world") 
+print(a+b)
+
+#Python Operatiors
+x = 9 y = 4 
+result_add= x + y # Addition 
+result_sub= x - y # Subtraction 
+result_mul= x * y # Multiplication 
+result_div= x / y # Division 
+result_fdiv= x // y # Floor Division 
+result_mod= x % y # Modulo</td>
+
+#Replace
+my_string="Hello" 
+new_text = my_string.replace("Hello", "Hi")
+
+
+#Slicing
+my_string="Hello" substring = my_string[0:5]
+
+#Split
+my_string="Hello" 
+split_text = my_string.split(",")
+
+#Strip
+my_string="Hello" 
+trimmed = my_string.strip()
+
+#Upper
+my_string="Hello" 
+uppercase_text = my_string.upper()
+
+#Variable Assgnment
+name="John" # assigning John to variable name 
+x = 5 # assigning 5 to variable x
+
