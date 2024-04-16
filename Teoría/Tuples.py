99e987a03072bfb3aac8edcbba08420f50604e66
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-04-15 20:16:46 -0500

    Create Tuples.py

---
+++ Teoría/Tuples.py
@@ -0,0 +1,13 @@
+Tuplas
+
+# Print element on each index, including nest indexes
+
+NestedT =(1, 2, ("pop", "rock") ,(3,4),("disco",(1,2)))
+
+print("Element 2, 0 of Tuple: ",   NestedT[2][0])
+print("Element 2, 1 of Tuple: ",   NestedT[2][1])
+print("Element 3, 0 of Tuple: ",   NestedT[3][0])
+print("Element 3, 1 of Tuple: ",   NestedT[3][1])
+print("Element 4, 0 of Tuple: ",   NestedT[4][0])
+print("Element 4, 1 of Tuple: ",   NestedT[4][1])
+
