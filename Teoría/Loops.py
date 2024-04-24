f1bc49713f25af008b795bb1087da36296b3bd8e
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-04-23 21:42:11 -0500

    Update Loops.py

--- Teoría/Loops.py
+++ Teoría/Loops.py
@@ -26,16 +26,13 @@ while count <= 10:
     print(count)
     count += 1
 
- 
-
-+
--
-Play
-
-
-00:00
-08:05
-Mute
+PlayListRatings = [10, 9.5, 10, 8, 7.5, 5, 10, 10]
+i = 0
+rating = PlayListRatings[0]
+while(i < len(PlayListRatings) and rating >= 6):
+    print(rating)
+    i = i + 1 
+    rating = PlayListRatings[i]   
 
 Settings
 cognitiveclass.ai logo
