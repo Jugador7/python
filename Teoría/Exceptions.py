99950941d669be86347efdf73a149de8a9286aea
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-04-25 21:03:36 -0500

    Update Exceptions.py

--- Teoría/Exceptions.py
+++ Teoría/Exceptions.py
@@ -3,6 +3,16 @@ result = 10 / 0
 print(result)
 # Raises ZeroDivisionError
 
+a = 1
+
+# using Try- except 
+try:
+    b = int(input("Please enter a number to divide a"))
+    a = a/b
+    print("Success a=",a)
+except:
+    print("There was an error")
+        
 # using Try- except 
 try:
     # Attempting to divide 10 by 0
@@ -41,3 +51,73 @@ length = len(text)  # No AttributeError, correct method usage
 missing = text.some_method()  # Raises AttributeError
 
 #This error is encountered when an attempt is made to import a module that is unavailable. For example: import non_existent_module
+
+
+#TRY EXCEPT ESCPECIFIC
+a = 1
+
+try:
+    b = int(input("Please enter a number to divide a"))
+    a = a/b
+    print("Success a=",a)
+except ZeroDivisionError:
+    print("The number you provided cant divide 1 because it is 0")
+except ValueError:
+    print("You did not provide a number")
+except:
+    print("Something went wrong")
+
+#TRY EXCEPT ELSE AND FINALLY
+
+# potential code before try catch
+
+try:
+    # code to try to execute
+except ZeroDivisionError:
+    # code to execute if there is a ZeroDivisionError
+except NameError:
+    # code to execute if there is a NameError
+except:
+    # code to execute if ther is any exception
+else:
+    # code to execute if there is no exception
+finally:
+    # code to execute at the end of the try except no matter what
+    
+# code that will execute if there is no exception or a one that we are handling
+
+a = 1
+
+try:
+    b = int(input("Please enter a number to divide a"))
+    a = a/b
+except ZeroDivisionError:
+    print("The number you provided cant divide 1 because it is 0")
+except ValueError:
+    print("You did not provide a number")
+except:
+    print("Something went wrong")
+else:
+    print("success a=",a)
+
+#GREAT EXAMPLE
+def safe_divide(numerator,denominator):
+    try:
+        result = numerator/denominator
+        return result
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero")
+        return None
+    except ValueError:
+        print("You did not provide a number")
+    except:
+        print("Something went wrong")
+    else:
+        print("success result=",result)
+    finally:
+        print("Processing Complete")
+        
+    
+numerator = int(input("Please enter a number to divide from"))
+denominator = int(input("Please enter a number to divide by"))
+print(safe_divide(numerator,denominator)) 
