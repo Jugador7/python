a7d6186ebd7cebcb371fc1ec9af06b3d07ba20c5
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-04-27 12:31:00 -0500

    Create ClassAnalyzeText.py

---
+++ Teoría/ClassAnalyzeText.py
@@ -0,0 +1,20 @@
+class TextAnalyzer(object):
+    
+    def __init__ (self, text):
+        # remove punctuation
+        formattedText = text.replace('.','').replace('!','').replace('?','').replace(',','')
+        
+        # make text lowercase
+        formattedText = formattedText.lower()
+        
+        self.fmtText = formattedText
+        
+    def freqAll(self):        
+        # split text into words
+        wordList = self.fmtText.split(' ')
+        
+        # Create dictionary
+        freqMap = {}
+        for word in set(wordList):
+                freqMap[word] = wordList.count(word)
+        return freqMap
