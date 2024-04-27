15e303a1c07130596682091f8842a63e8c789afb
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-04-27 11:48:51 -0500

    Update Classes.py

--- Teoría/Classes.py
+++ Teoría/Classes.py
@@ -78,3 +78,39 @@ object1.attribute2 = new_value  # Change the value of an attribute using dot not
 
 # Accessing class attributes (shared by all instances)
 class_attr_value = ClassName.class_attribute
+
+#Método para mostrar propiedades de un objeto. 
+    def show_properties(self):
+        print("Properties of the Vehicle:")
+        print(self.maximum_speed)
+        print(self.mileage)
+        print(self.seats)
+
+Example:
+
+class Vehicle:
+    color = "white"
+
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+        self.seating_capacity = None
+
+    def assign_seating_capacity(self, seating_capacity):
+        self.seating_capacity = seating_capacity
+
+    def display_properties(self):
+        print("Properties of the Vehicle:")
+        print("Color:", self.color)
+        print("Maximum Speed:", self.max_speed)
+        print("Mileage:", self.mileage)
+        print("Seating Capacity:", self.seating_capacity)
+
+# Creating objects of the Vehicle class
+vehicle1 = Vehicle(200, 20)
+vehicle1.assign_seating_capacity(5)
+vehicle1.display_properties()
+
+vehicle2 = Vehicle(180, 25)
+vehicle2.assign_seating_capacity(4)
+vehicle2.display_properties()
