bd5d946b5e508b8ba7b20b59cd32f918d7391ad0
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-05-01 10:58:14 -0500

    Update WritingFiles.py

--- Teoría/Files/WritingFiles.py
+++ Teoría/Files/WritingFiles.py
@@ -70,3 +70,124 @@ with open('/Example2.txt', 'w') as writefile:
     writefile.write("Overwrite\n")
 with open('/Example2.txt', 'r') as testwritefile:
     print(testwritefile.read())
+
+# Write line to file
+exmp2 = '/Example2.txt'
+with open(exmp2, 'w') as writefile:
+    writefile.write("This is line A")
+
+# Read file
+
+with open(exmp2, 'r') as testwritefile:
+    print(testwritefile.read())
+
+# Write lines to file
+
+with open(exmp2, 'w') as writefile:
+    writefile.write("This is line A\n")
+    writefile.write("This is line B\n")
+
+# Check whether write to file
+
+with open(exmp2, 'r') as testwritefile:
+    print(testwritefile.read())
+
+# Sample list of text
+
+Lines = ["This is line A\n", "This is line B\n", "This is line C\n"]
+Lines
+
+with open('/Example2.txt', 'w') as writefile:
+    for line in Lines:
+        print(line)
+        writefile.write(line)
+
+# Verify if writing to file is successfully executed
+
+with open('/Example2.txt', 'r') as testwritefile:
+    print(testwritefile.read())
+
+with open('/Example2.txt', 'w') as writefile:
+    writefile.write("Overwrite\n")
+with open('/Example2.txt', 'r') as testwritefile:
+    print(testwritefile.read())
+
+# Write a new line to text file
+
+with open('/Example2.txt', 'a') as testwritefile:
+    testwritefile.write("This is line C\n")
+    testwritefile.write("This is line D\n")
+    testwritefile.write("This is line E\n")
+
+# Verify if the new line is in the text file
+
+with open('/Example2.txt', 'r') as testwritefile:
+    print(testwritefile.read())
+
+with open('/Example2.txt', 'a+') as testwritefile:
+    testwritefile.write("This is line E\n")
+    print(testwritefile.read())
+
+#Move positions
+
+with open('/Example2.txt', 'a+') as testwritefile:
+    print("Initial Location: {}".format(testwritefile.tell()))
+    
+    data = testwritefile.read()
+    if (not data):  #empty strings return false in python
+            print('Read nothing') 
+    else: 
+            print(testwritefile.read())
+            
+    testwritefile.seek(0,0) # move 0 bytes from beginning.
+    
+    print("\nNew Location : {}".format(testwritefile.tell()))
+    data = testwritefile.read()
+    if (not data): 
+            print('Read nothing') 
+    else: 
+            print(data)
+    
+    print("Location after read: {}".format(testwritefile.tell()) )
+
+#Finally, a note on the difference between **w+** and **r+**. 
+#Both of these modes allow access to read and write methods, however, opening a file in **w+** overwrites it and deletes all pre-existing data. <br>
+#In the following code block, Run the code as it is first and then run it without the <code>.truncate()</code>.
+
+with open('/Example2.txt', 'r+') as testwritefile:
+    testwritefile.seek(0,0) #write at beginning of file
+    testwritefile.write("Line 1" + "\n")
+    testwritefile.write("Line 2" + "\n")
+    testwritefile.write("Line 3" + "\n")
+    testwritefile.write("Line 4" + "\n")
+    testwritefile.write("finished\n")
+    testwritefile.seek(0,0)
+    print(testwritefile.read())
+
+#To work with a file on existing data, use r+ and a+. 
+#While using r+, it can be useful to add a .truncate() method at the end of your data. 
+#This will reduce the file to your data and delete everything that follows.
+
+with open('/Example2.txt', 'r+') as testwritefile:
+    testwritefile.seek(0,0) #write at beginning of file
+    testwritefile.write("Line 1" + "\n")
+    testwritefile.write("Line 2" + "\n")
+    testwritefile.write("Line 3" + "\n")
+    testwritefile.write("Line 4" + "\n")
+    testwritefile.write("finished\n")
+    #Uncomment the line below
+    testwritefile.truncate()
+    testwritefile.seek(0,0)
+    print(testwritefile.read())
+
+# Copy file to another
+
+with open('/Example2.txt','r') as readfile:
+    with open('/Example3.txt','w') as writefile:
+          for line in readfile:
+                writefile.write(line)
+
+# Verify if the copy is successfully executed
+
+with open('/Example3.txt','r') as testwritefile:
+    print(testwritefile.read())
