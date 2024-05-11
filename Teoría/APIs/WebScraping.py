ae3f809a3f3e49000ec24760a5d25af6d31fd995
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-05-11 09:18:12 -0500

    Update WebScraping.py

--- Teoría/APIs/WebScraping.py
+++ Teoría/APIs/WebScraping.py
@@ -16,3 +16,28 @@ links = soup.find_all('a')
 # Iterate through the list of links and print their text
 for the link in links:
     print(link.text)
+
+####romperla con soup
+
+from bs4 import BeautifulSoup
+import requests
+URL = "http://www.example.com"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+
+###scrapy 
+
+import scrapy
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = ['http://quotes.toscrape.com/tag/humor/',]
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {'quote': quote.css('span.text::text').get()}
+
+##Selenium: Selenium is a tool used for controlling web browsers through programs and automating browser tasks.
+
+
+from selenium import webdriver
+driver = webdriver.Firefox()
+driver.get("http://www.example.com")
