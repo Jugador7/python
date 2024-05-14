d0a1dada9fcc447e88f725a9079ba277f8ca8a64
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-05-14 18:50:27 -0500

    Update Scraping.py

--- Final/Scraping.py
+++ Final/Scraping.py
@@ -22,9 +22,43 @@ data  = requests.get(url).text
 print(data)
 
 #Parse the HTML content of the web page using BeautifulSoup.
-
+soup = BeautifulSoup(data, 'html5lib')
 
 #Identify the HTML tags that contain the data you want to extract.
+netflix_data = pd.DataFrame(columns=["Date", "Open", "High", "Low", "Close", "Volume"])
+
 #Use BeautifulSoup methods to extract the data from the HTML tags.
+
+# First we isolate the body of the table which contains all the information
+# Then we loop through each row and find all the column values for each row
+for row in soup.find("tbody").find_all('tr'):
+    col = row.find_all("td")
+    date = col[0].text
+    Open = col[1].text
+    high = col[2].text
+    low = col[3].text
+    close = col[4].text
+    adj_close = col[5].text
+    volume = col[6].text
+    
+    # Finally we append the data of each row to the table
+    netflix_data = netflix_data.append({"Date":date, "Open":Open, "High":high, "Low":low, "Close":close, "Adj Close":adj_close, "Volume":volume}, ignore_index=True)    
+
 #Print the extracted data
+netflix_data.head()
+
+
+
+# Extracting data using `pandas` library
+
+read_html_pandas_data = pd.read_html(url)
+#Or you can convert the BeautifulSoup object to a string.
+read_html_pandas_data = pd.read_html(str(soup))
+
+#Because there is only one table on the page, just take the first table in the returned list.
+netflix_dataframe = read_html_pandas_data[0]
+
+netflix_dataframe.head()
+
+
 
