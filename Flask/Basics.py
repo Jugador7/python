3eda325e0424b76be595fec2de2ad09376628ff2
Jugador7 <moyagabriel14@gmail.com>
2024-05-20 08:36:26 -0500

    first flask steps

--- Flask/Basics.py
+++ Flask/Basics.py
@@ -21,20 +21,25 @@ def hello_world():
 #1. FLASK_APP (name of the main server file)
 #2. FLASK_ENV (se deprecará en Flask 2.3)
 
-export FLASK_APP=app.py
-export FLASK_ENV=development
+#En gitbash de Windows correr set FLASK_APP=app.py
+# En linux correr export FLASK_APP=app.py
+#export FLASK_ENV=development
 
 #Run the flask app
 
-flask run 
+#flask run 
 
 #(por defecto corre en puerto 5000)
 
 #Se puede pasar --app para identificar el archivo a correr
 #Se puede usar --debug para iniciar en modo debug (reinicia si los archivos cambiaron)
 
+
+#Mode debug
+#flask --app app --debug run
+
 #RETURNING JSON
-#Retornar un tipo serializable
+#1Retornar un tipo serializable
 
 from flask import Flask
 app = Flask(__name__)
@@ -43,6 +48,33 @@ app = Flask(__name__)
 def index():
   return {"message":"Hello world"}
 
+#2Usar el método Jsonify
+#pasar los pares llave:valor
+#retornar el JSON al cliente
 
+from flask import Flask,jsonify
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+  return jsonify (message = "Hello world")
 
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#ENV define el entorno
+#DEBUG
+#TESTING
+#SECRET_KEY firma la cookie de la sesión
+#SESSION_COOKIE_NAME
+#SERVER_NAME
+#JSONIFY_MIMETYPE
+
+#LoadApp configuration
+
+app.config["SECRET_KEY"] = "random-secret-key"
+
+#Configure from an environment variable
+
+app.config["VARIABLE_NAME"]
+app.config.from_prefixed_emv()
