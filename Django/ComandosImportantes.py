708aa10b95f0af16774d66c88f6246bb260b4cac
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-06-07 16:09:55 -0500

    Create ComandosImportantes.py

---
+++ Django/ComandosImportantes.py
@@ -0,0 +1,28 @@
+start_postgres
+
+Crear ambiente virtual
+pip install --upgrade distro-info
+pip3 install --upgrade pip==23.2.1
+pip install virtualenv
+virtualenv djangoenv
+source djangoenv/bin/activate
+pip install Django psycopg2-binary
+
+python3 manage.py makemigrations crud
+
+Esto nos debe dejar ver que se crearon las tablas que necesitamos. 
+
+python3 manage.py migrate 
+
+Con esto podemos ejecutar las migraciones. Con lo siguiente el Crud debería crearse OK
+
+Operations to perform:
+  Apply all migrations: crud
+Running migrations:
+  Applying crud.0001_initial... OK
+
+
+
+
+
+  
