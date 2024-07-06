5b08dc96d5ce0d88f125bb1fb418a9a9d544a2b0
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-07-06 08:55:29 -0500

    Update Lab1.py

--- DataWrangling/Lab1.py
+++ DataWrangling/Lab1.py
@@ -99,9 +99,157 @@ df['peak-rpm'].replace(np.nan, avg_peakrpm, inplace=True)
 
 #"num-of-doors": 2 missing data, replace them with "four".
 #Reason: 84% sedans are four doors. Since four doors is most frequent, it is most likely to occur
+#To see which values are present in a particular column, we can use the ".value_counts()" method:
+df['num-of-doors'].value_counts()
+#You can see that four doors is the most common type. We can also use the ".idxmax()" method to calculate the most common type automatically:
+df['num-of-doors'].value_counts().idxmax()
+#replace the missing 'num-of-doors' values by the most frequent 
+df["num-of-doors"].replace(np.nan, "four", inplace=True)
 
 #Drop the whole row:
-
 #"price": 4 missing data, simply delete the whole row
 #Reason: You want to predict price. You cannot use any data entry without price data for prediction; therefore any row now without price data is not useful to you.
+# simply drop whole row with NaN in "price" column
+df.dropna(subset=["price"], axis=0, inplace=True)
+# reset index, because we droped two rows
+df.reset_index(drop=True, inplace=True)
 
+#<h4>Let's list the data types for each column</h4>
+df.dtypes
+
+#<h4>Convert data types to proper format</h4>
+df[["bore", "stroke"]] = df[["bore", "stroke"]].astype("float")
+df[["normalized-losses"]] = df[["normalized-losses"]].astype("int")
+df[["price"]] = df[["price"]].astype("float")
+df[["peak-rpm"]] = df[["peak-rpm"]].astype("float")
+
+df.dtypes
+
+# Convert mpg to L/100km by mathematical operation (235 divided by mpg)
+df['city-L/100km'] = 235/df["city-mpg"]
+
+# check your transformed data 
+df.head()
+
+# transform mpg to L/100km by mathematical operation (235 divided by mpg)
+df["highway-mpg"] = 235/df["highway-mpg"]
+
+# rename column name from "highway-mpg" to "highway-L/100km"
+df.rename(columns={'"highway-mpg"':'highway-L/100km'}, inplace=True)
+
+# check your transformed data 
+df.head()
+
+#Normalization
+
+#Normalization is the process of transforming values of several variables into a similar range. Typical normalizations include
+
+#scaling the variable so the variable average is 0
+#scaling the variable so the variance is 1
+#scaling the variable so the variable values range from 0 to 1
+
+Example
+
+To demonstrate normalization, say you want to scale the columns "length", "width" and "height".
+
+Target: normalize those variables so their value ranges from 0 to 1
+
+Approach: replace the original value by (original value)/(maximum value)
+
+# replace (original value) by (original value)/(maximum value)
+df['length'] = df['length']/df['length'].max()
+df['width'] = df['width']/df['width'].max()
+df['height'] = df['height']/df['height'].max()
+#show the scaled columns
+df[["length","width","height"]].head()
+
+Binning
+Why binning?
+
+Binning is a process of transforming continuous numerical variables into discrete categorical 'bins' for grouped analysis.
+
+ Convert data to correct format:
+
+df["horsepower"]=df["horsepower"].astype(int, copy=True)
+
+%matplotlib inline
+import matplotlib as plt
+from matplotlib import pyplot
+plt.pyplot.hist(df["horsepower"])
+
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
+
+Find 3 bins of equal size bandwidth by using Numpy's linspace(start_value, end_value, numbers_generated function.
+Since you want to include the minimum value of horsepower, set start_value = min(df["horsepower"]).
+
+Since you want to include the maximum value of horsepower, set end_value = max(df["horsepower"]).
+
+Since you are building 3 bins of equal length, you need 4 dividers, so numbers_generated = 4.
+
+bins = np.linspace(min(df["horsepower"]), max(df["horsepower"]), 4)
+bins
+
+Set group  names:
+
+group_names = ['Low', 'Medium', 'High']
+df['horsepower-binned'] = pd.cut(df['horsepower'], bins, labels=group_names, include_lowest=True )
+df[['horsepower','horsepower-binned']].head(20)
+
+%matplotlib inline
+import matplotlib as plt
+from matplotlib import pyplot
+pyplot.bar(group_names, df["horsepower-binned"].value_counts())
+
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
+
+Normally, you use a histogram to visualize the distribution of bins we created above. 
+
+    %matplotlib inline
+import matplotlib as plt
+from matplotlib import pyplot
+
+
+# draw historgram of attribute "horsepower" with bins = 3
+plt.pyplot.hist(df["horsepower"], bins = 3)
+
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
+
+Indicator Variable
+df.columns
+
+dummy_variable_1 = pd.get_dummies(df["fuel-type"])
+dummy_variable_1.head()
+
+Change the column names for clarity
+dummy_variable_1.rename(columns={'gas':'fuel-type-gas', 'diesel':'fuel-type-diesel'}, inplace=True)
+dummy_variable_1.head()
+
+# merge data frame "df" and "dummy_variable_1" 
+df = pd.concat([df, dummy_variable_1], axis=1)
+
+# drop original column "fuel-type" from "df"
+df.drop("fuel-type", axis = 1, inplace=True)
+
+# Write your code below and press Shift+Enter to execute 
+dummy_variable_2 = pd.get_dummies(df["aspiration"])
+# change column names for clarity
+dummy_variable_2.rename(columns={'std':'aspiration-std', 'turbo': 'aspiration-turbo'}, inplace=True)
+dummy_variable_2.head()
+
+# Write your code below and press Shift+Enter to execute 
+# merge the new dataframe to the original datafram
+df = pd.concat([df, dummy_variable_2], axis=1)
+
+# drop original column "aspiration" from "df"
+df.drop('aspiration', axis = 1, inplace=True)
+
+df.head
