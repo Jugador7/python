899fadb6c0c7a4cb3bc9be2ec46933336d1fbd4d
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-07-06 14:32:08 -0500

    Update Lab2.py

--- ExploratoryDataAnalysis/Lab2.py
+++ ExploratoryDataAnalysis/Lab2.py
@@ -25,3 +25,54 @@ df = pd.read_csv(file_name, header=0)
 
 df.head(5)
 
+sns.regplot(x="CPU_frequency", y="Price", data=df)
+plt.ylim(0,)
+
+sns.regplot(x="Screen_Size_inch", y="Price", data=df)
+plt.ylim(0,)
+
+# Correlation values of the three attributes with Price
+for param in ["CPU_frequency", "Screen_Size_inch","Weight_pounds"]:
+    print(f"Correlation of Price and {param} is ", df[[param,"Price"]].corr())
+
+Categorical features
+sns.boxplot(x="Category", y="Price", data=df)
+
+#Generate the statistical description of all the features being used in the data set. Include "object" data types as well.
+
+print(df.describe())
+print(df.describe(include=['object']))
+
+#Grouping
+df_gptest = df[['GPU','CPU_core','Price']]
+grouped_test1 = df_gptest.groupby(['GPU','CPU_core'],as_index=False).mean()
+print(grouped_test1)
+
+#Grouping
+grouped_pivot = grouped_test1.pivot(index='GPU',columns='CPU_core')
+print(grouped_pivot)
+
+#Create plot
+fig, ax = plt.subplots()
+im = ax.pcolor(grouped_pivot, cmap='RdBu')
+
+#label names
+row_labels = grouped_pivot.columns.levels[1]
+col_labels = grouped_pivot.index
+
+#move ticks and labels to the center
+ax.set_xticks(np.arange(grouped_pivot.shape[1]) + 0.5, minor=False)
+ax.set_yticks(np.arange(grouped_pivot.shape[0]) + 0.5, minor=False)
+
+#insert labels
+ax.set_xticklabels(row_labels, minor=False)
+ax.set_yticklabels(col_labels, minor=False)
+
+fig.colorbar(im)
+
+#Use the scipy.stats.pearsonr() function to evaluate the Pearson Coefficient and the p-values for each parameter tested above. This will help you determine the parameters most likely to have a strong effect on the price of the laptops.
+
+for param in ['RAM_GB','CPU_frequency','Storage_GB_SSD','Screen_Size_inch','Weight_pounds','CPU_core','OS','GPU','Category']:
+    pearson_coef, p_value = stats.pearsonr(df[param], df['Price'])
+    print(param)
+    print("The Pearson Correlation Coefficient for ",param," is", pearson_coef, " with a P-value of P =", p_value)
