38f1d3a3d74d24cd1cd16e3afa3b18d91bfd2633
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-07-09 20:24:28 -0500

    Update Lab2.py

--- ModelDevelopment/Lab2.py
+++ ModelDevelopment/Lab2.py
@@ -39,4 +39,123 @@ df = pd.read_csv(file_name, header=0)
 print("The first 5 rows of the dataframe") 
 df.head(5)
 
+Task 1 : Single Linear Regression
+You have learnt that "CPU_frequency" is the parameter with the lowest p-value among the different features of the dataset. 
+Create a single feature Linear Regression model that fits the pair of "CPU_frequency" and "Price" to find the model for prediction.
 
+lm = LinearRegression()
+
+X = df[['CPU_frequency']]
+Y = df['price']
+
+lm.fit(X,Y)
+
+Yhat=lm.predict(X)
+
+Generate the Distribution plot for the predicted values and that of the actual values. How well did the model perform?
+
+ax1 = sns.distplot(df['Price'], hist=False, color="r", label="Actual Value")
+
+# Create a distribution plot for predicted values
+sns.distplot(Yhat, hist=False, color="b", label="Fitted Values" , ax=ax1)
+
+plt.title('Actual vs Fitted Values for Price')
+plt.xlabel('Price')
+plt.ylabel('Proportion of laptops')
+plt.legend(['Actual Value', 'Predicted Value'])
+plt.show()
+
+Evaluate the Mean Squared Error and R^2 score values for the model.
+mse_slr = mean_squared_error(df['Price'], Yhat)
+r2_score_slr = lm.score(X, Y)
+print('The R-square for Linear Regression is: ', r2_score_slr)
+print('The mean square error of price and predicted value is: ', mse_slr)
+
+Task 2 - Multiple Linear Regression
+The parameters which have a low enough p-value so as to indicate strong relationship with the 'Price' value are 'CPU_frequency', 'RAM_GB', 'Storage_GB_SSD', 'CPU_core', 'OS', 'GPU' and 'Category'. 
+Use all these variables to create a Multiple Linear Regression system.
+            
+lm1 = LinearRegression()
+Z = df[['CPU_frequency','RAM_GB','Storage_GB_SSD','CPU_core','OS','GPU','Category']]
+lm1.fit(Z,Y)
+Y_hat = lm1.predict(Z)
+
+     Plot the Distribution graph of the predicted values as well as the Actual values
+
+ax1 = sns.distplot(df['Price'], hist=False, color="r", label="Actual Value")
+sns.distplot(Y_hat, hist=False, color="b", label="Fitted Values" , ax=ax1)
+
+plt.title('Actual vs Fitted Values for Price')
+plt.xlabel('Price')
+plt.ylabel('Proportion of laptops')
+
+Find the R^2 score and the MSE value for this fit. Is this better or worst than the performance of Single Linear Regression?
+ax2 = sns.distplot(df['Price'], hist=False, color="r", label="Actual Value")
+sns.distplot(Y_hat, hist=False, color="b", label="Fitted Values" , ax=ax2)
+
+plt.title('Actual vs Fitted Values for Price')
+plt.xlabel('Price')
+plt.ylabel('Proportion of laptops')
+
+
+
+Task 3 - Polynomial Regression
+Use the variable "CPU_frequency" to create Polynomial features. Try this for 3 different values of polynomial degrees. Remember that polynomial fits are done using numpy.polyfit.
+
+X = X.to_numpy().flatten()
+f1 = np.polyfit(X, Y, 1)
+p1 = np.poly1d(f1)
+
+f3 = np.polyfit(X, Y, 3)
+p3 = np.poly1d(f3)
+
+f5 = np.polyfit(X, Y, 5)
+p5 = np.poly1d(f5)
+
+Plot the regression output against the actual data points to note how the data fits in each case. To plot the polynomial response over the actual data points, you have the function shown below.
+
+def PlotPolly(model, independent_variable, dependent_variabble, Name):
+    x_new = np.linspace(independent_variable.min(),independent_variable.max(),100)
+    y_new = model(x_new)
+
+    plt.plot(independent_variable, dependent_variabble, '.', x_new, y_new, '-')
+    plt.title(f'Polynomial Fit for Price ~ {Name}')
+    ax = plt.gca()
+    ax.set_facecolor((0.898, 0.898, 0.898))
+    fig = plt.gcf()
+    plt.xlabel(Name)
+    plt.ylabel('Price of laptops')
+
+Call this function for the 3 models created and get the required graphs.
+
+    PlotPolly(p1, X, Y, 'CPU_frequency')
+PlotPolly(p3, X, Y, 'CPU_frequency')
+    PlotPolly(p5, X, Y, 'CPU_frequency')
+
+Also, calculate the R^2 and MSE values for these fits. For polynomial functions, the function sklearn.metrics.r2_score will be used to calculate R^2 values.
+
+r_squared_1 = r2_score(Y, p1(X))
+print('The R-square value for 1st degree polynomial is: ', r_squared_1)
+print('The MSE value for 1st degree polynomial is: ', mean_squared_error(Y,p1(X)))
+r_squared_3 = r2_score(Y, p3(X))
+print('The R-square value for 3rd degree polynomial is: ', r_squared_3)
+print('The MSE value for 3rd degree polynomial is: ', mean_squared_error(Y,p3(X)))
+r_squared_5 = r2_score(Y, p5(X))
+print('The R-square value for 5th degree polynomial is: ', r_squared_5)
+print('The MSE value for 5th degree polynomial is: ', mean_squared_error(Y,p5(X)))
+           
+Task 4 - Pipeline
+Create a pipeline that performs parameter scaling, Polynomial Feature generation and Linear regression. Use the set of multiple features as before to create this pipeline.
+
+Input=[('scale',StandardScaler()), ('polynomial', PolynomialFeatures(include_bias=False)), ('model',LinearRegression())]
+pipe=Pipeline(Input)
+Z = Z.astype(float)
+pipe.fit(Z,Y)
+ypipe=pipe.predict(Z)
+
+print('MSE for multi-variable polynomial pipeline is: ', mean_squared_error(Y, ypipe))
+print('R^2 for multi-variable polynomial pipeline is: ', r2_score(Y, ypipe))
+
+
+    
+                                             
