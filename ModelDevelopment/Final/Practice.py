bef65d58ca04d7508625ee8248d3ace717d3e19b
Jugador7 <164368680+Jugador7@users.noreply.github.com>
2024-07-19 19:59:58 -0500

    Update Practice.py

--- ModelDevelopment/Final/Practice.py
+++ ModelDevelopment/Final/Practice.py
@@ -52,3 +52,191 @@ Note: This version of the lab is working on JupyterLite, which requires the data
 #df = pd.read_csv(filepath, header=None)
   
 Task 1 : Import the dataset¶
+
+Import the dataset into a pandas dataframe. Note that there are currently no headers in the CSV file.
+
+Print the first 10 rows of the dataframe to confirm successful loading.
+
+print(df.head(10))
+   19  1    27.9  0 1.1  3    16884.924
+0  18  2  33.770  1   0  4   1725.55230
+1  28  2  33.000  3   0  4   4449.46200
+2  33  2  22.705  0   0  1  21984.47061
+3  32  2  28.880  0   0  1   3866.85520
+4  31  1  25.740  0   ?  4   3756.62160
+5  46  1  33.440  1   0  4   8240.58960
+6  37  1  27.740  3   0  1   7281.50560
+7  37  2  29.830  2   0  2   6406.41070
+8  60  1  25.840  0   0  1  28923.13692
+9  25  2  26.220  0   0  2   2721.32080
+
+Add the headers to the dataframe, as mentioned in the project scenario.
+
+headers = ["age", "gender", "bmi", "no_of_children", "smoker", "region", "charges"]
+df.columns = headers
+
+Now, replace the '? entries with 'NaN' values.
+
+df.replace('?', np.nan, inplace = True)
+
+Task 2 : Data Wrangling
+Use dataframe.info() to identify the columns that have some 'Null' (or NaN) information.
+
+print(df.info())
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 2771 entries, 0 to 2770
+Data columns (total 7 columns):
+ #   Column          Non-Null Count  Dtype  
+---  ------          --------------  -----  
+ 0   age             2767 non-null   object 
+ 1   gender          2771 non-null   int64  
+ 2   bmi             2771 non-null   float64
+ 3   no_of_children  2771 non-null   int64  
+ 4   smoker          2764 non-null   object 
+ 5   region          2771 non-null   int64  
+ 6   charges         2771 non-null   float64
+dtypes: float64(2), int64(3), object(2)
+memory usage: 130.0+ KB
+None
+Click here for Solution
+print(df.info())
+Handle missing data:
+
+For continuous attributes (e.g., age), replace missing values with the mean.
+For categorical attributes (e.g., smoker), replace missing values with the most frequent value.
+Update the data types of the respective columns.
+Verify the update using df.info().
+Selection deleted
+# smoker is a categorical attribute, replace with most frequent entry
+is_smoker = df['smoker'].value_counts().idxmax()
+df["smoker"].replace(np.nan, is_smoker, inplace=True)
+
+# age is a continuous variable, replace with mean age
+mean_age = df['age'].astype('float').mean(axis=0)
+df["age"].replace(np.nan, mean_age, inplace=True)
+
+# Update data types
+df[["age","smoker"]] = df[["age","smoker"]].astype("int")
+
+print(df.info())
+Average age: 39.11709432598482
+Click here for Solution
+# smoker is a categorical attribute, replace with most frequent entry
+is_smoker = df['smoker'].value_counts().idxmax()
+df["smoker"].replace(np.nan, is_smoker, inplace=True)
+
+# age is a continuous variable, replace with mean age
+mean_age = df['age'].astype('float').mean(axis=0)
+df["age"].replace(np.nan, mean_age, inplace=True)
+
+# Update data types
+df[["age","smoker"]] = df[["age","smoker"]].astype("int")
+
+print(df.info())
+Also note, that the charges column has values which are more than 2 decimal places long. Update the charges column such that all values are rounded to nearest 2 decimal places. Verify conversion by printing the first 5 values of the updated dataframe.
+
+df[["charges"]] = np.round(df[["charges"]],2)
+print(df.head())
+Click here for Solution
+df[["charges"]] = np.round(df[["charges"]],2)
+print(df.head())
+
+
+
+Task 3 : Exploratory Data Analysis (EDA)
+Implement the regression plot for charges with respect to bmi.
+
+
+Click here for Solution
+sns.regplot(x="bmi", y="charges", data=df, line_kws={"color": "red"})
+plt.ylim(0,)
+Implement the box plot for charges with respect to smoker.
+
+
+Click here for Solution
+sns.boxplot(x="smoker", y="charges", data=df)
+Print the correlation matrix for the dataset.
+
+
+Click here for Solution
+print(df.corr())
+
+
+
+Task 4 : Model Development
+Fit a linear regression model that may be used to predict the charges value, just by using the smoker attribute of the dataset. Print the 
+ score of this model.
+
+Selection deleted
+
+Click here for Solution
+X = df[['smoker']]
+Y = df['charges']
+lm = LinearRegression()
+lm.fit(X,Y)
+print(lm.score(X, Y))
+Fit a linear regression model that may be used to predict the charges value, just by using all other attributes of the dataset. Print the 
+ score of this model. You should see an improvement in the performance.
+
+Selection deleted
+
+Click here for Solution
+# definition of Y and lm remain same as used in last cell. 
+Z = df[["age", "gender", "bmi", "no_of_children", "smoker", "region"]]
+lm.fit(Z,Y)
+print(lm.score(Z, Y))
+Create a training pipeline that uses StandardScaler(), PolynomialFeatures() and LinearRegression() to create a model that can predict the charges value using all the other attributes of the dataset. There should be even further improvement in the performance.
+
+Selection deleted
+
+Click here for Solution
+# Y and Z use the same values as defined in previous cells 
+Input=[('scale',StandardScaler()), ('polynomial', PolynomialFeatures(include_bias=False)), ('model', LinearRegression())]
+pipe=Pipeline(Input)
+Z = Z.astype(float)
+pipe.fit(Z,Y)
+ypipe=pipe.predict(Z)
+print(r2_score(Y,ypipe))
+
+
+ask 5 : Model Refinement
+Split the data into training and testing subsets, assuming that 20% of the data will be reserved for testing.
+
+
+Click here for Solution
+# Z and Y hold same values as in previous cells
+x_train, x_test, y_train, y_test = train_test_split(Z, Y, test_size=0.2, random_state=1)
+Initialize a Ridge regressor that used hyperparameter 
+. Fit the model using training data data subset. Print the 
+ score for the testing data.
+
+
+Click here for Solution
+# x_train, x_test, y_train, y_test hold same values as in previous cells
+RidgeModel=Ridge(alpha=0.1)
+RidgeModel.fit(x_train, y_train)
+yhat = RidgeModel.predict(x_test)
+print(r2_score(y_test,yhat))
+Apply polynomial transformation to the training parameters with degree=2. Use this transformed feature set to fit the same regression model, as above, using the training subset. Print the 
+ score for the testing subset.
+
+
+Click here for Solution
+# x_train, x_test, y_train, y_test hold same values as in previous cells
+pr = PolynomialFeatures(degree=2)
+x_train_pr = pr.fit_transform(x_train)
+x_test_pr = pr.fit_transform(x_test)
+RidgeModel.fit(x_train_pr, y_train)
+y_hat = RidgeModel.predict(x_test_pr)
+print(r2_score(y_test,y_hat))
+
+
+     
+
+
+
+
+
+
+
+
